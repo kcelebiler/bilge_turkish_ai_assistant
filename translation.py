720884659c827e8062ea3fd0cd586d9c7427ecd6
Kerim Gorkem Celebiler <kerim_celebiler@hotmail.com>
2021-08-20 22:46:53 +0300

    added translate function

--- translation.py
+++ translation.py
@@ -1,3 +1,5 @@
+from googletrans import Translator
+
 supported_langs={
         "ingilizce" : "en",
         "fransızca" : "fr",
@@ -6,3 +8,11 @@ supported_langs={
         "arapça" : "ar",
         "rusça" : "ru"
 }
+
+translator = Translator()
+
+def translate(text, target):
+    if target in supported_langs.keys():
+        return translator.translate(text, dest=supported_langs[target]).text
+    else:
+        return "Bu dil desteklenmiyor"
