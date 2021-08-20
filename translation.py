0f7e017cd470c5a559b3ad372728f6b015f82984
Kerim Gorkem Celebiler <kerim_celebiler@hotmail.com>
2021-08-20 22:40:47 +0300

    added translation module with the supported languages dict

---
+++ translation.py
@@ -0,0 +1,8 @@
+supported_langs={
+        "ingilizce" : "en",
+        "fransızca" : "fr",
+        "almanca" : "de",
+        "ispanyolca" : "es",
+        "arapça" : "ar",
+        "rusça" : "ru"
+}
