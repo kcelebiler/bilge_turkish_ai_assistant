f26c8646a514e8df26e6f90bf2c6bf08d1b45593
Kerim Gorkem Celebiler <kerim_celebiler@hotmail.com>
2021-08-20 22:32:44 +0300

    relocated codes into the functions

--- bilge.py
+++ bilge.py
@@ -7,6 +7,20 @@ import pygame
 from time import sleep
 import pyglet
 
+def recognize_wakeword(audio):
+    
+    return r.recognize_google(audio, language='tr-TR')
+
+def assistant_speak():
+    tts = gTTS(text='Buyrun benim adım bilge...', lang='tr')
+        
+    filename = 'temp.mp3'
+    tts.save(filename)
+    music = pyglet.media.load(filename, streaming=False)
+    music.play()
+    sleep(music.duration)
+    os.remove(filename)
+
 r = sr.Recognizer()
 
 mic = sr.Microphone(device_index=1)
@@ -16,15 +30,8 @@ while True:
     with mic as source:
         audio = r.listen(source)
 
-    detected_speech = r.recognize_google(audio, language='tr-TR')
+    detected_speech = recognize_wakeword(audio)
     if detected_speech.lower() == "hey bilge":
-        tts = gTTS(text='Buyrun benim adım bilge...', lang='tr')
-        
-        filename = 'temp.mp3'
-        tts.save(filename)
-        music = pyglet.media.load(filename, streaming=False)
-        music.play()
-        sleep(music.duration)
-        os.remove(filename)
+        assistant_speak()
         break
         
